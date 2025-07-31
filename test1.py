96c43f8411839a95d38a3b18dcf001dd4ee50cd6
Maa1kc <i@mklimachev.ru>
2025-07-31 17:30:11 +0300

    teest

--- test1.py
+++ test1.py
@@ -468,3 +468,47 @@ queries = [
 for q in queries:
     result = check_query(q)
     print(q, '—', result)
+
+    DATABASE = {
+        'Серёга': 'Омск',
+        'Соня': 'Москва',
+        'Миша': 'Москва',
+        'Дима': 'Челябинск',
+        'Алина': 'Красноярск',
+        'Егор': 'Пермь',
+        'Коля': 'Красноярск'
+    }
+
+
+    def process_anfisa(query):
+        if query == 'Сколько у меня друзей?':
+            count = len(DATABASE)
+
+            return 'У тебя ' + str(count) + ' друзей.'
+        elif query == 'Кто все мои друзья?':
+            # Из словаря DATABASE создайте строку с помощью join();
+            # имена друзей разделите запятой и пробелом.
+            # Запишите эту строку в переменную friends_string (вместо пустых кавычек).
+            friends_string = ', '.join(list(DATABASE))
+
+            # Этот цикл больше не понадобится, удалите его
+
+            return 'Твои друзья: ' + friends_string
+        elif query == 'Где все мои друзья?':
+            unique_cities = set(DATABASE.values())
+            # Из сета unique_cities создайте строку с помощью join();
+            # названия городов разделите запятой и пробелом.
+            # Запишите эту строку в переменную cities_string (вместо пустых кавычек).
+            cities_string = ', '.join(unique_cities)
+
+            # Этот цикл больше не понадобится, удалите его
+
+            return 'Твои друзья в городах: ' + cities_string
+        else:
+            return '<неизвестный запрос>'
+
+
+    print('Привет, я Анфиса!')
+    print(process_anfisa('Сколько у меня друзей?'))
+    print(process_anfisa('Кто все мои друзья?'))
+    print(process_anfisa('Где все мои друзья?'))
